02c67e1ce7f57793c598fe2417048fde6297fa4e
Micky Snadden <mick.taige@gmail.com>
2024-02-18 12:44:07 +1300

    Add updated js file

--- index.user.js
+++ index.user.js
@@ -1,118 +1,177 @@
 // ==UserScript==
-// @name        New script 
+// @name        VUW Timesheet Time Saver
 // @namespace   Violentmonkey Scripts
 // @match       https://vuwp.ascenderpay.com/ords/*
 // @grant       none
-// @version     1.0
-// @author      -
+// @version     0.1
+// @author      fushSauce
 // @require https://cdnjs.cloudflare.com/ajax/libs/ical.js/1.5.0/ical.min.js
-// @require https://cdn.jsdelivr.net/npm/dayjs@1/dayjs.min.js
-// @require https://cdn.jsdelivr.net/npm/dayjs@1/plugin/customParseFormat.js
-// @description 16/01/2024, 14:57:38
+// @description Copy your calendar events directly into your VUW timesheet.
 // ==/UserScript==
-
-// console.log("hello world!")
-
-// function doYaBusiness() {
-//     general_timesheet_button = document.querySelector("span[title='General Timesheet']")
-//     if (general_timesheet_button !== null) {
-//         general_timesheet_button.click()
-//     }
-//     add_timesheet_button = document.querySelector("body p a")
-//     if (add_timesheet_button !== null && add_timesheet_button.innerText === "Click here to add a new timesheet") {
-//         add_timesheet_button.click()
-//     }
-//     start_date_input = document.querySelector("input[name='P_START_DATE']")
-//     if (start_date_input !== null) {
-//         start_date_input.value = '15-Jan-2024'
-//     }
-//     find_employee_jobs_button = document.querySelector("input[value='Find Employee Jobs']")
-//     if (find_employee_jobs_button !== null) {
-//         find_employee_jobs_button.click()
-//     }
-// }
-
-// doYaBusiness()
-
-tsentry = document.querySelectorAll("tbody#TSEntry tr")
-
-foo = () => {
-    const ical_string = prompt("Enter iCalendar Data")
-    const ical = ICAL.parse(ical_string)
-    const comp = new ICAL.Component(ical);
-
-    if (tsentry.length === 0) {
-        throw new Error("No Entries!")
+// *********************
+// Get Data
+// *********************
+/**
+ * Listen for paste and check if we can parse it as ical data, if so, insert
+ * the entries we get from ical data.
+ */
+addEventListener('paste', function (event) {
+    var pastedData = event.clipboardData.getData('text/plain');
+    var rows = document.querySelectorAll('tbody#TSEntry tr');
+    try {
+        // @ts-expect-error Since Userscript, we use @require icaljs lib but typescript doesn't know
+        ICAL.parse(pastedData);
     }
-
-    subcomponents = comp.getAllSubcomponents("vevent")
-    our_entries = []
-    for (let subComponentIndex in subcomponents) {
-
-        sc = new ICAL.Event(subcomponents[subComponentIndex])
-        startDate = sc.startDate
-        endDate = sc.endDate
-
-        let shortMonth = new Date(Date.UTC(startDate.year,startDate.month-1,startDate.day-1,startDate.hour,startDate.minute,startDate.second)).toLocaleString('en-nz', { month: 'short', timeZone: 'Pacific/Auckland' }); /* Jun */
-        workDate = `${startDate.day.toString().padStart(2,'0')}-${shortMonth}-${startDate.year}`
-
-        let shortWeekday = new Date(Date.UTC(startDate.year,startDate.month-1,startDate.day-1,startDate.hour,startDate.minute,startDate.second)).toLocaleString('en-nz', { weekday: 'short', timeZone: 'Pacific/Auckland' }); /* Jun */
-
-        startDate = new Date(Date.UTC(startDate.year,startDate.month-1,startDate.day-1,startDate.hour,startDate.minute,startDate.second))
-
-        endDate = new Date(Date.UTC(endDate.year,endDate.month-1,endDate.day-1,endDate.hour,endDate.minute,endDate.second))
-
-        nHours = (((endDate.getTime()-startDate.getTime())/1000)/60)/60
-
-        startTime = startDate.toISOString().substring(11,16)
-        endTime = endDate.toISOString().substring(11,16)
-
-        our_entries.push({workDate:workDate,day:shortWeekday,startTime:startTime,finishTime:endTime,breakLength:0,units:nHours,payCode:"ORDHR",activity:sc.summary})
+    catch (e) {
+        // Ignore if not ical data
+        return;
     }
-    console.log("our Entries: ",our_entries);
-
-    for (let entryIndex in our_entries) {
-        entry = our_entries[entryIndex]
-        insertEntry(entry,tsentry)
+    // only insert entries if we were able to parse ical data hence return in catch.
+    for (var _i = 0, _a = icalToEntryList(pastedData); _i < _a.length; _i++) {
+        var entry = _a[_i];
+        insertEntryValues(entry, rows);
     }
-}
-
-cancelButton = document.querySelector("input[value=\"Cancel\"]")
-cancelButton.insertAdjacentHTML("afterend",'&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" value="Pass iCal Data" onclick="foo()">')
-
-insertEntry = (entry,rows) => {
-    // find nearest empty row
-    nearest_empty_row = null
-    for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
-        row = rows[rowIndex]
-        work_date_input = row.querySelector("input[name='P_WORK_DATE']").value
-        console.log("type: ",typeof(work_date_input))
-        if (!work_date_input) {
-            nearest_empty_row = row
-            break
+});
+// *********************
+// Process
+// *********************
+var Entry = /** @class */ (function () {
+    function Entry() {
+    }
+    return Entry;
+}());
+/**
+ * Convert ical data to a list of entries
+ * @param icalData ical data that is pasted by user
+ * @returns list of entries
+ */
+var icalToEntryList = function (icalData) {
+    // @ts-expect-error Since Userscript, we use @require icaljs lib but typescript doesn't know
+    var ical = ICAL.parse(icalData);
+    // @ts-expect-error Since Userscript, we use @require icaljs lib but typescript doesn't know
+    var comp = new ICAL.Component(ical);
+    var subcomponents = comp.getAllSubcomponents('vevent');
+    // map ical events into entries which only contain data we need
+    return subcomponents.map(function (event) {
+        // @ts-expect-error Since Userscript, we use @require icaljs lib but typescript doesn't know
+        event = new ICAL.Event(event);
+        var startDate = convertDate(event.startDate);
+        var endDate = convertDate(event.endDate);
+        var startDateTime = event.startDate._time;
+        var date = new Date(startDateTime.year, startDateTime.month - 1, startDateTime.day, startDateTime.hour, startDateTime.minute, startDateTime.second);
+        var shortMonth = date.toLocaleString('en-nz', { month: 'short' }); /* Jun */
+        var workDate = "".concat(date.getDate()
+            .toString()
+            // @ts-expect-error idk bruh pad start be whilin
+            .padStart(2, '0'), "-").concat(shortMonth, "-").concat(date.getFullYear());
+        var day = date.toLocaleString('en-NZ', { weekday: 'short' });
+        var units = (endDate.getTime() - startDate.getTime()) / 1000 / 60 / 60;
+        var startTime = startDate.toISOString().substring(11, 16);
+        var finishTime = endDate.toISOString().substring(11, 16);
+        var activity = event.summary;
+        var breakLength = 0;
+        var payCode = 'ORDHR';
+        var dayNumber = date.getDay();
+        return {
+            workDate: workDate,
+            day: day,
+            date: date,
+            dayNumber: dayNumber,
+            startTime: startTime,
+            finishTime: finishTime,
+            breakLength: breakLength,
+            units: units,
+            payCode: payCode,
+            activity: activity
+        };
+    }).sort(function (entry1, entry2) {
+        if (entry1.date.getTime() < entry2.date.getTime()) {
+            return -1;
+        }
+        else if (entry1.date.getTime() < entry2.date.getTime()) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    });
+};
+/**
+ * Convert ical.js date to vanilla JS Date to get delta in dates more easily.
+ * @param icaljsDate
+ * @returns JS Date instance
+ */
+var convertDate = function (icaljsDate) {
+    return new Date(Date.UTC(icaljsDate.year, 
+    // -1 because JS Date uses 0 indexed months and days
+    icaljsDate.month - 1, icaljsDate.day - 1, icaljsDate.hour, icaljsDate.minute, icaljsDate.second));
+};
+var findNearestEmptyRow = function (rows) {
+    var nearestEmptyRow;
+    for (var _i = 0, rows_1 = rows; _i < rows_1.length; _i++) {
+        var row = rows_1[_i];
+        var workDateInput = row.querySelector("input[name='P_WORK_DATE']").value;
+        if (workDateInput === '') {
+            nearestEmptyRow = row;
+            break;
         }
     }
-    if (nearest_empty_row === null) {
-        throw new Error("Couldn't find empty row!")
+    if (nearestEmptyRow === null) {
+        throw new Error("Couldn't find empty row!");
     }
-    console.log("entry: ",entry)
-    console.log("ner: ",nearest_empty_row)
-
-    work_date_input = nearest_empty_row.querySelector("input#P_WORK_DATE")
-    start_time_input = nearest_empty_row.querySelector("input#P_START_TIME")
-    finish_time_input = nearest_empty_row.querySelector("input#P_FINISH_TIME")
-    break_input = nearest_empty_row.querySelector("input#P_BREAK")
-    pay_code_input = nearest_empty_row.querySelector("input#P_PAYCODE")
-    activity_input = nearest_empty_row.querySelector("input#P_TOPIC_DETAILS")
-    units_input = nearest_empty_row.querySelector("input#P_UNITS")
-    day_input = nearest_empty_row.querySelector("input#P_DAY")
-
-    work_date_input.value = entry.workDate
-    start_time_input.value = entry.startTime
-    finish_time_input.value = entry.finishTime
-    break_input.value = entry.breakLength
-    pay_code_input.value = entry.payCode
-    activity_input.value = entry.activity
-    units_input.value = entry.units
-    day_input.value = entry.day
+    return nearestEmptyRow;
+};
+/**
+ * Finds nearest empty row and populates that row with the entry data.
+ * @param entry
+ * @param rows
+ */
+var insertEntryValues = function (entry, rows) {
+    var nearestEmptyRow = findNearestEmptyRow(rows);
+    // each input in the row has id and the entry property name that corresponds to it
+    var idToEntryProperty = {
+        'input#P_WORK_DATE': 'workDate',
+        'input#P_START_TIME': 'startTime',
+        'input#P_FINISH_TIME': 'finishTime',
+        'input#P_BREAK': 'breakLength',
+        'input#P_PAYCODE': 'payCode',
+        'input#P_TOPIC_DETAILS': 'activity',
+        'input#P_UNITS': 'units',
+        'input#P_DAY': 'day'
+    };
+    // iterate inputs and set the values using the entry properties.
+    Object.keys(idToEntryProperty).forEach(function (s) {
+        // get given input area
+        var inputArea = nearestEmptyRow.querySelector(s);
+        // set value in input area to the corresponding entry property value
+        inputArea.value = entry[idToEntryProperty[s]];
+    });
+};
+// *********************
+// Debugging
+// *********************
+/**
+ * Takes ages to manually get to timesheet page when debugging, this automates
+ * it, just add the date you want the timesheet to start at.
+ * ex usage: debugGetToTimesheetPage('11-FEB-2024')
+ * @param defaultDate
+ */
+function debugGetToTimesheetPage(defaultDate) {
+    var generalTimesheetButton = document.querySelector("span[title='General Timesheet']");
+    if (generalTimesheetButton !== null) {
+        generalTimesheetButton.click();
+    }
+    var addTimesheetButton = document.querySelector('body p a');
+    if (addTimesheetButton !== null &&
+        addTimesheetButton.innerText === 'Click here to add a new timesheet') {
+        addTimesheetButton.click();
+    }
+    var startDateInput = document.querySelector("input[name='P_START_DATE']");
+    if (startDateInput !== null) {
+        startDateInput.value = defaultDate;
+    }
+    var findEmployeeJobsButton = document.querySelector("input[value='Find Employee Jobs']");
+    if (findEmployeeJobsButton !== null) {
+        findEmployeeJobsButton.click();
+    }
 }
